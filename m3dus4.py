012212ccc204b9522ed88d40fe842475ef3438ad
Luan Simões <timpsembs.7@gmail.com>
2021-07-11 21:41:06 -0300

    add omnisint

--- m3dus4.py
+++ m3dus4.py
@@ -1,6 +1,6 @@
 from modules.helpers import remove_duplicates_from_lists, save_to_file, make_directory
 import modules.input as input
-from subdomains import assetfinder, sublist3r
+from subdomains import assetfinder, sublist3r, omnisint
 from misc import logo, waybackurls
 from scanners import httprobe, aquatone
 
@@ -13,12 +13,13 @@ def run():
     for domain in domains:
         directory = make_directory(domain)
 
+        omnisint_subs = omnisint.run(domain)
         sublist3r_subs = sublist3r.run(domain)
         assetf_subs = assetfinder.run(domain)
         wayback_subs = waybackurls.run(domain)
 
         unified_domains = remove_duplicates_from_lists(
-            sublist3r_subs, assetf_subs, wayback_subs)
+            sublist3r_subs, assetf_subs, wayback_subs, omnisint_subs)
 
         live_subdomains = httprobe.run(unified_domains)
         live_subdomains_file = save_to_file(directory, live_subdomains)
--- scanners/aquatone.py
+++ scanners/aquatone.py
@@ -3,4 +3,4 @@ import subprocess
 
 def run(alive_subdomains, folder_to_save):
     subprocess.run(
-        f'cat {alive_subdomains} | aquatone -out {folder_to_save} -chrome-path snap/bin/chromium -silent', shell=True)
+        f'cat {alive_subdomains} | aquatone -out {folder_to_save} -chrome-path /snap/bin/chromium -silent', shell=True)
