fce6e36d077b9c3164bf4f580c947b0075c09e10
Luan Simões <timpsembs.7@gmail.com>
2021-07-15 21:41:38 -0300

    fix bug on index out of range

--- misc/waybackurls.py
+++ misc/waybackurls.py
@@ -8,19 +8,24 @@ BASE_URL = 'http://web.archive.org/cdx/search/cdx?url=*.{}/*&output=json&fl=orig
 def run(domain):
     def get_information(domain):
 
-        r = requests.get(BASE_URL.format(domain))
-        results = r.json()
-        return results[1:]
+        try:
+            r = requests.get(BASE_URL.format(domain))
+            results = r.json()
+            return results[1:]
+        except requests.ConnectionError:
+            return None
 
     def get_subdomains(results):
         subdomains = set()
 
         # split('/')[2])
-
-        for i in results:
-            for j in i:
-                subdomains.add(str(j).split('/')[2])
-        return subdomains
+        try:
+            for i in results:
+                for j in i:
+                    subdomains.add(str(j).split('/')[2])
+            return subdomains
+        except IndexError:
+            print('List out of range')
 
     results = get_information(domain)
     subdomains = get_subdomains(results)
